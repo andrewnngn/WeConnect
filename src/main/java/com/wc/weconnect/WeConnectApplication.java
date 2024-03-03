fbfb5c745fc2e00e58fa0f7b8806f951219883c0
andrew <coder@gmail.com>
2024-03-03 21:18:37 +0700

    Initial commit

---
+++ src/main/java/com/wc/weconnect/WeConnectApplication.java
@@ -0,0 +1,13 @@
+package com.wc.weconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeConnectApplication.class, args);
+	}
+
+}
