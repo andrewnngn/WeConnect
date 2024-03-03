1cf04fb8eeada87fbba20a279b1b300813ddc812
andrew <coder@gmail.com>
2024-03-03 21:33:20 +0700

    init post model

--- src/main/java/com/wc/weconnect/WeConnectApplication.java
+++ src/main/java/com/wc/weconnect/WeConnectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeConnectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeConnectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeConnectApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/wc/weconnect/models/Post.java
@@ -0,0 +1,2 @@
+package com.wc.weconnect.models;public class Post {
+}
