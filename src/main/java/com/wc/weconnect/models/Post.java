4cdfcb491607dc2873a9b14900e9cf9719e6b319
andrew <coder@gmail.com>
2024-03-04 20:19:06 +0700

    init message model

--- src/main/java/com/wc/weconnect/models/Post.java
+++ src/main/java/com/wc/weconnect/models/Post.java
@@ -1,2 +1,44 @@
-package com.wc.weconnect.models;public class Post {
+package com.wc.weconnect.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="posts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String caption;
+
+    private String image;
+
+    private String video;
+
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany
+    private List<User> liked = new ArrayList<>();
+
+    private LocalDateTime createdAt;
+
+    @OneToMany
+    private List<Comment> comments = new ArrayList<>();
+
+
 }
