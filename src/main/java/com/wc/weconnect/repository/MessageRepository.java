65fe0a6c28730328ff6e6f93280d2b47e9303c12
andrew <coder@gmail.com>
2024-03-08 21:32:39 +0700

    add message repo

---
+++ src/main/java/com/wc/weconnect/repository/MessageRepository.java
@@ -0,0 +1,12 @@
+package com.WeConnect.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.WeConnect.models.Message;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+	
+	public List<Message> findByChatId(Integer chatId);
+}
